ecdf76d5d24aa4f7aa921d3e77a53237819b68d8
Akash Kumar <akashkumar59200@gmail.com>
2021-02-02 22:38:19 +0530

    commit on master

---
+++ UseAssignmentOperators2.java
@@ -0,0 +1,15 @@
+public class UseAssignmentOperators2 {
+
+    public static void main(String[] args) {
+        int i = 10;
+
+        i += 2;
+        i -= 5;
+        i *= 6;
+
+        System.out.println(i);
+        System.out.println(i += 3);
+        System.out.println(i /= 2);
+    }
+    
+}
